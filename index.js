4cae0d431572fd38a4b7f9fa46826257b2c4859a
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2024-09-02 23:23:51 -0500

    add time functions

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
-const express = require("express");
-const app = express();
-require("dotenv").config();
+import * as dotenv from "dotenv";
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
+import cors from "cors";
+import { StatusCodes } from "http-status-codes";
 
-const cors = require("cors");
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+dotenv.config();
+const app = express();
+
+app.use(cors({ optionsSuccessStatus: 200 }));
 
 app.use(express.static("public"));
+app.use(express.json());
 
-app.get("/", function (req, res) {
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-const listener = app.listen(process.env.PORT || 3000, function () {
+app.get("/api", (req, res) => {
+  let current = new Date(Date.now());
+  return res
+    .status(StatusCodes.OK)
+    .json({ unix: current.getTime(), utc: current.toUTCString() });
+});
+
+app.get("/api/:date_string", (req, res) => {
+  const { date_string } = req.params;
+  let time = new Date(date_string);
+  if (!isNaN(date_string)) {
+    time = new Date(date_string * 1000);
+  }
+  res
+    .status(StatusCodes.OK)
+    .json({ unix: time.getTime(), utc: time.toUTCString() });
+});
+
+const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
